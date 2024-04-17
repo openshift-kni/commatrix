liornoy/node-comm-lib

modify query to filter

internal/endpointslices/endpointslices.go
@@ -8,12 +8,108 @@ import (
 	discoveryv1 "k8s.io/api/discovery/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
+	rtclient "sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/liornoy/node-comm-lib/internal/client"
 	"github.com/liornoy/node-comm-lib/internal/consts"
 	"github.com/liornoy/node-comm-lib/internal/nodes"
 	"github.com/liornoy/node-comm-lib/internal/types"
 )
+
+type EndpointSliceInfo struct {
+	endpointSlice discoveryv1.EndpointSlice
+	serivce       corev1.Service
+	pods          []corev1.Pod
+}
+
+func GetIngressEndpointSlices(cs *client.ClientSet) ([]EndpointSliceInfo, error) {
+	var (
+		epSlicesList discoveryv1.EndpointSliceList
+		servicesList corev1.ServiceList
+		podsList     corev1.PodList
+	)
+
+	err := cs.List(context.TODO(), &epSlicesList, &rtclient.ListOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list endpointslices: %w", err)
+	}
+
+	err = cs.List(context.TODO(), &servicesList, &rtclient.ListOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list services: %w", err)
+	}
+
+	err = cs.List(context.TODO(), &podsList, &rtclient.ListOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list pods: %w", err)
+	}
+
+	epsliceInfo, err := createEndpointSliceInfo(epSlicesList, servicesList, podsList)
+	if err != nil {
+		return nil, fmt.Errorf("failed to bundle resources: %w", err)
+	}
+
+	res := FilterForIngressTraffic(epsliceInfo)
+
+	return res, nil
+}
+func createEndpointSliceInfo(epSlicesList discoveryv1.EndpointSliceList, servicesList corev1.ServiceList, podsList corev1.PodList) ([]EndpointSliceInfo, error) {
+	var service corev1.Service
+	var pod corev1.Pod
+	var found bool
+	res := make([]EndpointSliceInfo, len(epSlicesList.Items))
+
+	for _, epSlice := range epSlicesList.Items {
+
+		pods := make([]corev1.Pod, 1)
+
+		// Fetch info about the service behind the endpointslice.
+		for _, ownerRef := range epSlice.OwnerReferences {
+			name := ownerRef.Name
+			namespace := epSlice.Namespace
+			if service, found = getService(name, namespace, servicesList); !found {
+				return nil, fmt.Errorf("failed to get service for endpoint %s/%s", epSlice.Namespace, epSlice.Name)
+			}
+		}
+
+		// Fetch info about the pods behind the endpointslice.
+		for _, endpoint := range epSlice.Endpoints {
+			name := endpoint.TargetRef.Name
+			namespace := endpoint.TargetRef.Namespace
+
+			if pod, found = getPod(name, namespace, podsList); !found {
+				return nil, fmt.Errorf("failed to get service for endpoint %s/%s", epSlice.Namespace, epSlice.Name)
+			}
+			pods = append(pods, pod)
+		}
+
+		res = append(res, EndpointSliceInfo{
+			endpointSlice: epSlice,
+			serivce:       service,
+			pods:          pods,
+		})
+	}
+
+	return res, nil
+}
+func getPod(name, namespace string, podsList corev1.PodList) (corev1.Pod, bool) {
+	for _, pod := range podsList.Items {
+		if pod.Name == name && pod.Namespace == namespace {
+			return pod, true
+		}
+	}
+	return corev1.Pod{}, false
+}
+
+func getService(name, namespace string, serviceList corev1.ServiceList) (corev1.Service, bool) {
+	for _, service := range serviceList.Items {
+		if service.Name == name && service.Namespace == namespace {
+			return service, true
+		}
+	}
+
+	return corev1.Service{}, false
+}
 
 func toComDetails(cs *client.ClientSet, epSlice discoveryv1.EndpointSlice) ([]types.ComDetails, error) {
 	res := make([]types.ComDetails, 0)
@@ -59,10 +155,10 @@ func toComDetails(cs *client.ClientSet, epSlice discoveryv1.EndpointSlice) ([]ty
 	return res, nil
 }
 
-func ToComDetails(cs *client.ClientSet, slices []discoveryv1.EndpointSlice) ([]types.ComDetails, error) {
+func ToComDetails(cs *client.ClientSet, epSlicesInfo []EndpointSliceInfo) ([]types.ComDetails, error) {
 	ComDetails := make([]types.ComDetails, 0)
-	for _, epSlice := range slices {
-		cds, err := toComDetails(cs, epSlice)
+	for _, epSliceInfo := range epSlicesInfo {
+		cds, err := toComDetails(cs, epSliceInfo)
 		if err != nil {
 			return nil, err
 		}

internal/endpointslices/filter_test.go
@@ -1,0 +1,80 @@
+package endpointslices
+
+import (
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+	discoveryv1 "k8s.io/api/discovery/v1"
+)
+
+func TestFilterHostNetwork(t *testing.T) {
+	epInfo := EndpointSliceInfo{
+		endpointSlice: discoveryv1.EndpointSlice{},
+		pods:          []corev1.Pod{{}},
+		serivce:       corev1.Service{},
+	}
+	tests := []struct {
+		desc          string
+		isHostNetwork bool
+		expected      bool
+	}{
+		{
+			desc:          "with-host-network",
+			isHostNetwork: true,
+			expected:      true,
+		},
+		{
+			desc:          "without-host-network",
+			isHostNetwork: false,
+			expected:      false,
+		},
+	}
+	for _, test := range tests {
+		epInfo.pods[0].Spec.HostNetwork = test.isHostNetwork
+		res := FilterHostNetwork(epInfo)
+		if res != test.expected {
+			t.Fatalf("test %s failed. expected %v got %v", test.desc, test.expected, res)
+		}
+	}
+}
+
+func TestFilterServiceTypes(t *testing.T) {
+	epInfo := EndpointSliceInfo{
+		endpointSlice: discoveryv1.EndpointSlice{},
+		pods:          []corev1.Pod{},
+		serivce:       corev1.Service{},
+	}
+	tests := []struct {
+		desc        string
+		serviceType corev1.ServiceType
+		expected    bool
+	}{
+		{
+			desc:        "with-service-type-loadbalancer",
+			serviceType: corev1.ServiceTypeLoadBalancer,
+			expected:    true,
+		},
+		{
+			desc:        "with-service-type-node-port",
+			serviceType: corev1.ServiceTypeNodePort,
+			expected:    true,
+		},
+		{
+			desc:        "with-service-type-cluster-ip",
+			serviceType: corev1.ServiceTypeClusterIP,
+			expected:    false,
+		},
+		{
+			desc:        "with-service-type-external-name",
+			serviceType: corev1.ServiceTypeExternalName,
+			expected:    false,
+		},
+	}
+	for _, test := range tests {
+		epInfo.serivce.Spec.Type = test.serviceType
+		res := FilterServiceTypes(epInfo)
+		if res != test.expected {
+			t.Fatalf("test %s failed. expected %v got %v", test.desc, test.expected, res)
+		}
+	}
+}

openshift-kni/commatrix

internal/endpointslices/endpointslices.go
@@ -3,17 +3,119 @@ package endpointslices
 import (
 	"context"
 	"fmt"
+	"log"
 
 	corev1 "k8s.io/api/core/v1"
 	discoveryv1 "k8s.io/api/discovery/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
+	rtclient "sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/liornoy/node-comm-lib/internal/client"
 	"github.com/liornoy/node-comm-lib/internal/consts"
 	"github.com/liornoy/node-comm-lib/internal/nodes"
 	"github.com/liornoy/node-comm-lib/internal/types"
 )
+
+type SvcInfo struct {
+	endpointSlice discoveryv1.EndpointSlice
+	serivce       corev1.Service
+	pods          []corev1.Pod
+}
+
+func GetIngressEndpointSlices(cs *client.ClientSet) ([]SvcInfo, error) {
+	var (
+		epSlicesList discoveryv1.EndpointSliceList
+		servicesList corev1.ServiceList
+		podsList     corev1.PodList
+	)
+
+	err := cs.List(context.TODO(), &epSlicesList, &rtclient.ListOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list endpointslices: %w", err)
+	}
+
+	err = cs.List(context.TODO(), &servicesList, &rtclient.ListOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list services: %w", err)
+	}
+
+	err = cs.List(context.TODO(), &podsList, &rtclient.ListOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list pods: %w", err)
+	}
+
+	epsliceInfo, err := createEndpointSliceInfo(epSlicesList, servicesList, podsList)
+	if err != nil {
+		return nil, fmt.Errorf("failed to bundle resources: %w", err)
+	}
+
+	res := FilterForIngressTraffic(epsliceInfo)
+
+	return res, nil
+}
+func createEndpointSliceInfo(epSlicesList discoveryv1.EndpointSliceList, servicesList corev1.ServiceList, podsList corev1.PodList) ([]SvcInfo, error) {
+	var service corev1.Service
+	var pod corev1.Pod
+	var found bool
+	res := make([]SvcInfo, len(epSlicesList.Items))
+
+	for _, epSlice := range epSlicesList.Items {
+
+		pods := make([]corev1.Pod, 0)
+
+		// Fetch info about the service behind the endpointslice.
+		for _, ownerRef := range epSlice.OwnerReferences {
+			name := ownerRef.Name
+			namespace := epSlice.Namespace
+			if service, found = getService(name, namespace, servicesList); !found {
+				return nil, fmt.Errorf("failed to get service for endpoint %s/%s", epSlice.Namespace, epSlice.Name)
+			}
+		}
+
+		// Fetch info about the pods behind the endpointslice.
+		for _, endpoint := range epSlice.Endpoints {
+			if endpoint.TargetRef == nil {
+				continue
+			}
+			name := endpoint.TargetRef.Name
+			namespace := endpoint.TargetRef.Namespace
+
+			if pod, found = getPod(name, namespace, podsList); !found {
+				log.Printf("warning: failed to get service for endpoint %s/%s", epSlice.Namespace, epSlice.Name)
+				continue
+				//return nil, fmt.Errorf("failed to get service for endpoint %s/%s", epSlice.Namespace, epSlice.Name)
+			}
+			pods = append(pods, pod)
+		}
+
+		res = append(res, SvcInfo{
+			endpointSlice: epSlice,
+			serivce:       service,
+			pods:          pods,
+		})
+	}
+
+	return res, nil
+}
+func getPod(name, namespace string, podsList corev1.PodList) (corev1.Pod, bool) {
+	for _, pod := range podsList.Items {
+		if pod.Name == name && pod.Namespace == namespace {
+			return pod, true
+		}
+	}
+	return corev1.Pod{}, false
+}
+
+func getService(name, namespace string, serviceList corev1.ServiceList) (corev1.Service, bool) {
+	for _, service := range serviceList.Items {
+		if service.Name == name && service.Namespace == namespace {
+			return service, true
+		}
+	}
+
+	return corev1.Service{}, false
+}
 
 func toComDetails(cs *client.ClientSet, epSlice discoveryv1.EndpointSlice) ([]types.ComDetails, error) {
 	res := make([]types.ComDetails, 0)
@@ -59,10 +161,10 @@ func toComDetails(cs *client.ClientSet, epSlice discoveryv1.EndpointSlice) ([]ty
 	return res, nil
 }
 
-func ToComDetails(cs *client.ClientSet, slices []discoveryv1.EndpointSlice) ([]types.ComDetails, error) {
+func ToComDetails(cs *client.ClientSet, epSlicesInfo []SvcInfo) ([]types.ComDetails, error) {
 	ComDetails := make([]types.ComDetails, 0)
-	for _, epSlice := range slices {
-		cds, err := toComDetails(cs, epSlice)
+	for _, epSliceInfo := range epSlicesInfo {
+		cds, err := toComDetails(cs, epSliceInfo.endpointSlice)
 		if err != nil {
 			return nil, err
 		}

internal/endpointslices/filter_test.go
@@ -1,0 +1,80 @@
+package endpointslices
+
+import (
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+	discoveryv1 "k8s.io/api/discovery/v1"
+)
+
+func TestFilterHostNetwork(t *testing.T) {
+	epInfo := SvcInfo{
+		endpointSlice: discoveryv1.EndpointSlice{},
+		pods:          []corev1.Pod{{}},
+		serivce:       corev1.Service{},
+	}
+	tests := []struct {
+		desc          string
+		isHostNetwork bool
+		expected      bool
+	}{
+		{
+			desc:          "with-host-network",
+			isHostNetwork: true,
+			expected:      true,
+		},
+		{
+			desc:          "without-host-network",
+			isHostNetwork: false,
+			expected:      false,
+		},
+	}
+	for _, test := range tests {
+		epInfo.pods[0].Spec.HostNetwork = test.isHostNetwork
+		res := FilterHostNetwork(epInfo)
+		if res != test.expected {
+			t.Fatalf("test %s failed. expected %v got %v", test.desc, test.expected, res)
+		}
+	}
+}
+
+func TestFilterServiceTypes(t *testing.T) {
+	epInfo := SvcInfo{
+		endpointSlice: discoveryv1.EndpointSlice{},
+		pods:          []corev1.Pod{},
+		serivce:       corev1.Service{},
+	}
+	tests := []struct {
+		desc        string
+		serviceType corev1.ServiceType
+		expected    bool
+	}{
+		{
+			desc:        "with-service-type-loadbalancer",
+			serviceType: corev1.ServiceTypeLoadBalancer,
+			expected:    true,
+		},
+		{
+			desc:        "with-service-type-node-port",
+			serviceType: corev1.ServiceTypeNodePort,
+			expected:    true,
+		},
+		{
+			desc:        "with-service-type-cluster-ip",
+			serviceType: corev1.ServiceTypeClusterIP,
+			expected:    false,
+		},
+		{
+			desc:        "with-service-type-external-name",
+			serviceType: corev1.ServiceTypeExternalName,
+			expected:    false,
+		},
+	}
+	for _, test := range tests {
+		epInfo.serivce.Spec.Type = test.serviceType
+		res := FilterServiceTypes(epInfo)
+		if res != test.expected {
+			t.Fatalf("test %s failed. expected %v got %v", test.desc, test.expected, res)
+		}
+	}
+}