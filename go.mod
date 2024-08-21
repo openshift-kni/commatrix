e4685b8e087c27438e31e02aa1a39051def77fe3
aabughosh <88486034+aabughosh@users.noreply.github.com>
2024-08-21 15:58:19 +0300

    add a test for the ssGenrate function on listeningsockets

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gocarina/gocsv v0.0.0-20231116093920-b87c2d0e983a
 	github.com/golang/mock v1.6.0
 	github.com/sirupsen/logrus v1.9.3
+	github.com/stretchr/testify v1.8.4
 	golang.org/x/sync v0.5.0
 	k8s.io/api v0.29.3
 	k8s.io/apimachinery v0.29.3
@@ -35,6 +36,7 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
+	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.8.0 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
@@ -56,6 +58,7 @@ require (
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/mxk/go-flowrate v0.0.0-20140419014527-cca7078d478f // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/oauth2 v0.12.0 // indirect
---
+++ pkg/listening-sockets/listening-sockets_test.go
@@ -0,0 +1,139 @@
+package listeningsockets
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/openshift-kni/commatrix/pkg/client"
+	"github.com/openshift-kni/commatrix/pkg/consts"
+	mock_utils "github.com/openshift-kni/commatrix/pkg/utils/mock"
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	fakek "k8s.io/client-go/kubernetes/fake"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func normalizeOutput(s string) string {
+	s = strings.ReplaceAll(s, "\t", " ")
+	s = strings.ReplaceAll(s, "\n ", "\n") // Handle newlines with leading spaces
+	s = strings.TrimSpace(s)
+	return s
+}
+
+func TestGenerateSS(t *testing.T) {
+	sch := runtime.NewScheme()
+
+	if err := v1.AddToScheme(sch); err != nil {
+		t.Fatalf("failed to add corev1 to scheme: %v", err)
+	}
+
+	testNode := &v1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-node",
+			Labels: map[string]string{
+				"node-role.kubernetes.io/master": "", // Label to mark this node as a master
+			},
+		},
+	}
+
+	fakeClient := fake.NewClientBuilder().WithScheme(sch).WithObjects(testNode).Build()
+
+	fakeClientset := fakek.NewSimpleClientset()
+
+	clientset := &client.ClientSet{
+		Client:          fakeClient,
+		CoreV1Interface: fakeClientset.CoreV1(),
+	}
+
+	ctrlTest := gomock.NewController(t)
+	defer ctrlTest.Finish()
+
+	mockUtils := mock_utils.NewMockUtilsInterface(ctrlTest)
+
+	tcpOutput := []byte(`LISTEN 0      4096      127.0.0.1:8797  0.0.0.0:* users:(("machine-config-",pid=3534,fd=3))                
+LISTEN 0      4096      127.0.0.1:8798  0.0.0.0:* users:(("machine-config-",pid=3534,fd=13))               
+LISTEN 0      4096      127.0.0.1:9100  0.0.0.0:* users:(("node_exporter",pid=4147,fd=3))`)
+
+	udpOutput := []byte(`UNCONN 0      0           0.0.0.0:111   0.0.0.0:* users:(("rpcbind",pid=1399,fd=5),("systemd",pid=1,fd=78))
+UNCONN 0      0         127.0.0.1:323   0.0.0.0:* users:(("chronyd",pid=1015,fd=5))                        
+UNCONN 0      0      10.46.97.104:500   0.0.0.0:* users:(("pluto",pid=2115,fd=21))`)
+
+	output := []byte(`1: /system.slice/crio-1234567890abcdef.scope
+2: /system.slice/other-service.scope
+3: /system.slice/sshd.service`)
+
+	mockUtils.EXPECT().RunCommandOnPod(gomock.Any(), gomock.Any()).DoAndReturn(
+		func(pod *v1.Pod, cmd []string) ([]byte, error) {
+			if len(cmd) > 2 && strings.HasPrefix(cmd[2], "crictl ps -o json --id") {
+				return []byte(`{
+					"containers": [
+						{
+							"labels": {
+								"io.kubernetes.container.name": "test-container",
+								"io.kubernetes.pod.name": "test-pod",
+								"io.kubernetes.pod.namespace": "test-namespace"
+							}
+						}
+					]
+				}`), nil
+			}
+			if strings.HasPrefix(cmd[2], "cat /proc/") && strings.Contains(cmd[2], "/cgroup") {
+				return output, nil
+			}
+			if cmd[2] == "ss -anpltH" {
+				return tcpOutput, nil
+			}
+			if cmd[2] == "ss -anpluH" {
+				return udpOutput, nil
+			}
+			return nil, fmt.Errorf("unknown command")
+		},
+	).AnyTimes()
+
+	mockUtils.EXPECT().
+		CreateNamespace(consts.DefaultDebugNamespace).
+		Return(nil).
+		AnyTimes()
+
+	mockUtils.EXPECT().
+		DeleteNamespace(consts.DefaultDebugNamespace).
+		Return(nil).
+		AnyTimes()
+
+	mockPod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mock-pod",
+			Namespace: "mock-namespace",
+		},
+	}
+
+	mockUtils.EXPECT().
+		CreatePodOnNode(gomock.Any(), consts.DefaultDebugNamespace, consts.DefaultDebugPodImage).
+		Return(mockPod, nil).AnyTimes()
+
+	mockUtils.EXPECT().DeletePod(mockPod).Return(nil).AnyTimes()
+
+	connectionCheck, err := NewCheck(clientset, mockUtils, "/some/dest/dir")
+	assert.NoError(t, err)
+
+	ssMat, ssOutTCP, ssOutUDP, err := connectionCheck.GenerateSS()
+
+	expectedTCPOutput := `node: test-node
+LISTEN 0      4096      127.0.0.1:8797  0.0.0.0:* users:(("machine-config-",pid=3534,fd=3))                
+LISTEN 0      4096      127.0.0.1:8798  0.0.0.0:* users:(("machine-config-",pid=3534,fd=13))               
+LISTEN 0      4096      127.0.0.1:9100  0.0.0.0:* users:(("node_exporter",pid=4147,fd=3))`
+
+	expectedUDPOutput := `node: test-node
+UNCONN 0      0           0.0.0.0:111   0.0.0.0:* users:(("rpcbind",pid=1399,fd=5),("systemd",pid=1,fd=78))
+UNCONN 0      0         127.0.0.1:323   0.0.0.0:* users:(("chronyd",pid=1015,fd=5))                        
+UNCONN 0      0      10.46.97.104:500   0.0.0.0:* users:(("pluto",pid=2115,fd=21))`
+
+	assert.NoError(t, err)
+	assert.NotNil(t, ssMat)
+	assert.Equal(t, normalizeOutput(expectedTCPOutput), normalizeOutput(string(ssOutTCP)))
+	assert.Equal(t, normalizeOutput(expectedUDPOutput), normalizeOutput(string(ssOutUDP)))
+}
