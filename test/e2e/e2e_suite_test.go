59266ffa338b9cc2d78baaa69fe07ec6c99882a1
aabughosh <aabugosh@redhat.com>
2025-05-04 15:34:59 +0300

    add PLATFORM_TYPE to the generate command PLATFORM_TYPE was added to the generate command so after updating the commatrix krew version need to update the e2e test to use the required var update the in makefile the call of e2e-test to be with platform-type update the commands on the code to use the required var if the value not set will fail with relevant msg

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -8,6 +8,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/openshift-kni/commatrix/cmd/generate"
 	"github.com/openshift-kni/commatrix/pkg/client"
 	"github.com/openshift-kni/commatrix/pkg/utils"
 	corev1 "k8s.io/api/core/v1"
@@ -20,9 +21,12 @@ var (
 	utilsHelpers utils.UtilsInterface
 	nodeList     *corev1.NodeList
 	artifactsDir string
+	platformType string
 )
 
-const testNS = "openshift-commatrix-test"
+const (
+	testNS = "openshift-commatrix-test"
+)
 
 var _ = BeforeSuite(func() {
 	kubeconfig := os.Getenv("KUBECONFIG")
@@ -48,8 +52,16 @@ var _ = BeforeSuite(func() {
 	isSNO, err = utilsHelpers.IsSNOCluster()
 	Expect(err).NotTo(HaveOccurred())
 
-	isBM, err = utilsHelpers.IsBMInfra()
-	Expect(err).NotTo(HaveOccurred())
+	isBM = false
+	platformType, _ = os.LookupEnv("PLATFORM_TYPE")
+	if platformType == "" || (platformType != string(generate.PlatformBaremetal) && platformType != string(generate.PlatformAWS)) { // if not set or value is wrong will fail
+		log.Println("Environment variable PLATFORM_TYPE is not set or has an invalid value; please set it to either 'baremetal' or 'aws'")
+		Fail("Invalid or unset PLATFORM_TYPE environment variable")
+	}
+
+	if platformType == string(generate.PlatformBaremetal) { // if it set
+		isBM = true
+	}
 })
 
 func TestE2e(t *testing.T) {
